a0025ad098bc2eccb15a0ccb0a28e956c297f5ff
Kallaf <43083774+Kallaf@users.noreply.github.com>
2019-05-11 14:54:18 +0200

    Create Find Digits.js

---
+++ Algorithms/implementation/Easy/Find Digits.js
@@ -0,0 +1,55 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the findDigits function below.
+function findDigits(n) {
+    //const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+    
+    var count = 0, m = n;
+    while (n)
+    {
+        var d = n % 10;
+        if (d && !(m % d)) count++;
+        n = Math.floor(n/10);
+    }
+    return count;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const t = parseInt(readLine(), 10);
+
+    for (let tItr = 0; tItr < t; tItr++) {
+        const n = parseInt(readLine(), 10);
+
+        let result = findDigits(n);
+
+        ws.write(result + "\n");
+    }
+
+    ws.end();
+}
