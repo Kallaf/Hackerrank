0fb8392e6e971a1a43a2244d50d218cd58f5c128
Kallaf <43083774+Kallaf@users.noreply.github.com>
2019-05-27 13:39:37 +0200

    Create Repeated String.js

---
+++ Algorithms/implementation/Easy/Repeated String.js
@@ -0,0 +1,54 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the repeatedString function below.
+function repeatedString(s, n) {
+    var countA = 0, countReminder = 0;
+    for (let i = 0; i < s.length; i++)
+    {
+        if (s[i] === 'a')
+        {
+            countA++;
+            if (i < n % s.length)
+                countReminder++;
+        }
+    }
+    return countA * Math.floor(n / s.length) + countReminder;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const s = readLine();
+
+    const n = parseInt(readLine(), 10);
+
+    let result = repeatedString(s, n);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
