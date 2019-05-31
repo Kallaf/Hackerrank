1fd7016e9711c4be7f8d12ce32add97f2da61136
Kallaf <43083774+Kallaf@users.noreply.github.com>
2019-05-31 17:30:24 +0200

    Create Sparse Arrays.js

---
+++ Data structures/Arrays/Sparse Arrays.js
@@ -0,0 +1,75 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the matchingStrings function below.
+function matchingStrings(strings, queries) {
+    var count = {};
+    for (let i = 0; i < strings.length; i++)
+    {
+        if (count[strings[i]] == undefined)
+            count[strings[i]] = 1;
+        else
+            count[strings[i]]++;
+    }
+
+    var ans = [];
+    for (let i = 0; i < queries.length; i++)
+    {
+        if (count[queries[i]] == undefined)
+            ans.push(0);
+        else
+            ans.push(count[queries[i]]);
+    }
+    return ans;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const stringsCount = parseInt(readLine(), 10);
+
+    let strings = [];
+
+    for (let i = 0; i < stringsCount; i++) {
+        const stringsItem = readLine();
+        strings.push(stringsItem);
+    }
+
+    const queriesCount = parseInt(readLine(), 10);
+
+    let queries = [];
+
+    for (let i = 0; i < queriesCount; i++) {
+        const queriesItem = readLine();
+        queries.push(queriesItem);
+    }
+
+    let res = matchingStrings(strings, queries);
+
+    ws.write(res.join("\n") + "\n");
+
+    ws.end();
+}
