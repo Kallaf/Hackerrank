57d8b5c732b274f0f80a083cc351897dd0768e0c
Kallaf <43083774+Kallaf@users.noreply.github.com>
2019-07-17 09:18:22 +0200

    Create Running Time of Algorithms.cpp

---
+++ Algorithms/Sorting/Running Time of Algorithms.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main()
+{
+    int n,shifts = 0;
+    cin >> n;
+    vector<int> arr(n);
+    for(int i=0;i<n;i++)
+        cin >> arr[i];
+    for (int i = 1; i < n; i++) 
+    {  
+        int key = arr[i],j = i - 1;  
+        while (j >= 0 && arr[j] > key) 
+        {  
+            arr[j + 1] = arr[j];  
+            j = j - 1;
+            shifts++;  
+        }  
+        arr[j + 1] = key;  
+    } 
+    cout << shifts << endl;
+    return 0;
+}
