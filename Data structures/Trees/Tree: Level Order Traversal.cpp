54bd3accaa9fdc8111dd11a240239d6ec2a1fe9a
Ahmed Elkalaf <kalaf1997@yahoo.com>
2019-11-05 14:47:12 +0200

    Create Tree: Level Order Traversal.cpp

---
+++ Data structures/Trees/Tree: Level Order Traversal.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>	
+
+using namespace std;
+
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+class Solution {
+    public:
+  		Node* insert(Node* root, int data) {
+            if(root == NULL) {
+                return new Node(data);
+            } else {
+                Node* cur;
+                if(data <= root->data) {
+                    cur = insert(root->left, data);
+                    root->left = cur;
+                } else {
+                    cur = insert(root->right, data);
+                    root->right = cur;
+               }
+
+               return root;
+           }
+        }
+/*
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+*/
+
+    void levelOrder(Node * root) {
+        queue<Node*> q;
+        q.push(root);
+        while(!q.empty())
+        {
+            Node* node = q.front();
+            q.pop();
+            printf("%d ",node->data);
+            if(node->left)q.push(node->left);
+            if(node->right)q.push(node->right);
+        }
+
+    }
+
+}; //End of Solution
