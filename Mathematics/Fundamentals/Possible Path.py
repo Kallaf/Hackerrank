8101bee739e23204235f19b73c4c537df6be51b5
Ahmed Elkalaf <kalaf1997@yahoo.com>
2019-11-16 04:08:40 +0200

    Create Possible Path.py

---
+++ Mathematics/Fundamentals/Possible Path.py
@@ -0,0 +1,30 @@
+#!/bin/python3
+
+import os
+import sys
+import math
+
+# Complete the solve function below.
+def solve(a, b, x, y):
+    return "YES" if math.gcd(a,b) == math.gcd(x,y) else "NO"
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input())
+
+    for t_itr in range(t):
+        abxy = input().split()
+
+        a = int(abxy[0])
+
+        b = int(abxy[1])
+
+        x = int(abxy[2])
+
+        y = int(abxy[3])
+
+        result = solve(a, b, x, y)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
